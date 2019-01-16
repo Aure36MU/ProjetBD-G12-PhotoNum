5122b468209a2da39d82b611c7434bc34bfa10e1
allemanp <pauline71380@sfr.fr>
2019-01-16 16:20:48 +0100

    ajout attribut package Impression

--- src/impression/Album.java
+++ src/impression/Album.java
@@ -1,5 +1,14 @@
 package src.impression;
 
 public class Album extends Impression {
+	private String titreCouv;
 
+	public String getTitreCouv() {
+		return titreCouv;
+	}
+
+	public void setTitreCouv(String titreCouv) {
+		this.titreCouv = titreCouv;
+	}
+	
 }
--- src/impression/Impression.java
+++ src/impression/Impression.java
@@ -1,5 +1,45 @@
 package src.impression;
 
 public abstract class Impression {
+	private int idImp;
+	private int nbrPages;
+	private EnumQualite qualite;
+	private EnumFormat format;
 
+	public enum EnumQualite{
+		BASSE,
+		MEDIUM,
+		PREMIUM
+	}
+	public enum EnumFormat{
+		PETIT,
+		MOYEN,
+		GRAND
+	}
+	public int getIdImp() {
+		return idImp;
+	}
+	public void setIdImp(int idImp) {
+		this.idImp = idImp;
+	}
+	public int getNbrPages() {
+		return nbrPages;
+	}
+	public void setNbrPages(int nbrPages) {
+		this.nbrPages = nbrPages;
+	}
+	public EnumQualite getQualite() {
+		return qualite;
+	}
+	public void setQualite(EnumQualite qualite) {
+		this.qualite = qualite;
+	}
+	public EnumFormat getFormat() {
+		return format;
+	}
+	public void setFormat(EnumFormat format) {
+		this.format = format;
+	}
+	
+	
 }
