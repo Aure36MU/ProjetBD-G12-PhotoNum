0317b166feda931fd97f1acc07bd2fba724cec44
Aure36MU <32073804+Aure36MU@users.noreply.github.com>
2019-01-18 11:47:31 +0100

    CalendrierDAO : Refactoring + ajout méthode selectAllFromUser() la méthode getCalendriers() permet de construire les Calendriers à partir d'un résultat de requête

--- src/impression/calendrier/CalendrierDAO.java
+++ src/impression/calendrier/CalendrierDAO.java
@@ -11,14 +11,87 @@ import src.impression.Qualite;
 
 public class CalendrierDAO {
 	
+	/**
+	 * Sélectionne tous les calendriers (quels que soient leurs types : Bureau, Mural...) sans conditions.
+	 * 
+	 * @param conn Connection SQL
+	 * @return ArrayList contenant tous les objets Calendrier
+	 */
 	public static ArrayList<Calendrier> selectAll(Connection conn) {
 		
-		ArrayList<Calendrier> calendriers = new ArrayList<Calendrier>();
 		try {
 			conn.setAutoCommit(true);
 	
 			Statement state = conn.createStatement();
 			ResultSet result = state.executeQuery("SELECT * FROM Calendrier;");
+			return CalendrierDAO.getCalendriers(result);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+	
+	/**
+	 * Sélectionne tous les calendriers (quels que soient leurs types : Bureau, Mural...) sans conditions.
+	 * 
+	 * @param conn Connection SQL
+	 * @param condition chaîne de caractères formaté comme suit : "condition1 {AND condition2}"
+	 * Exemple : "foo=1 AND bar='bar' AND truc<>42"
+	 * @return ArrayList contenant les objets Calendrier sélectionnés
+	 */
+	public static ArrayList<Calendrier> selectAll(Connection conn, String condition) {
+			
+		try {
+			conn.setAutoCommit(true);
+	
+			Statement state = conn.createStatement();
+			ResultSet result = state.executeQuery("SELECT * FROM Calendrier WHERE "+condition+";");
+			return CalendrierDAO.getCalendriers(result);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	/**
+	 * Sélectionne tous les Calendriers créés par un certain utilisateur.
+	 * 
+	 * @param conn Connection SQL
+	 * @param id id utilisateur
+	 * @return ArrayList contenant les objets Calendrier sélectionnés
+	 */
+	public static ArrayList<Calendrier> selectAllFromUser(Connection conn, int id) {
+		
+		try {
+			conn.setAutoCommit(true);
+	
+			Statement state = conn.createStatement();
+			ResultSet result = state.executeQuery("SELECT * FROM Calendrier WHERE idUser="+id+";");
+			return CalendrierDAO.getCalendriers(result);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	/**
+	 * Retourne les objets Calendrier construits à partir d'un résultat de requête.
+	 * 
+	 * @param result le ResultSet de la requête SQL
+	 * @return ArrayList contenant les objets Calendrier
+	 */
+	public static ArrayList<Calendrier> getCalendriers(ResultSet result) {
+		ArrayList<Calendrier> calendriers = new ArrayList<Calendrier>();
+		try {
 			while (result.next()) {
 				if (result.getString("modele").equalsIgnoreCase("Bureau")) {
 					calendriers.add(new CalendrierBureau(
@@ -40,55 +113,13 @@ public class CalendrierDAO {
 					
 				}				
 			}
-			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		}
 		return calendriers;
-		
 	}
-	
-		
-	public static ArrayList<Calendrier> selectAll(Connection conn, String condition) {
-			
-			ArrayList<Calendrier> calendriers = new ArrayList<Calendrier>();
-			try {
-				conn.setAutoCommit(true);
-		
-				Statement state = conn.createStatement();
-				ResultSet result = state.executeQuery("SELECT * FROM Calendrier WHERE "+condition+";");
-				while (result.next()) {
-					if (result.getString("modele").equalsIgnoreCase("Bureau")) {
-						calendriers.add(new CalendrierBureau(
-								result.getInt("idImp"),
-								(Qualite) result.getObject("qualite"),
-								(Format) result.getObject("format"),
-								result.getInt("idUser"),
-								result.getInt("nbrPageTotal")
-								));
-						
-					} else if (result.getString("modele").equalsIgnoreCase("Mural")){
-						calendriers.add(new CalendrierMural(
-								result.getInt("idImp"),
-								(Qualite) result.getObject("qualite"),
-								(Format) result.getObject("format"),
-								result.getInt("idUser"),
-								result.getInt("nbrPageTotal")
-								));
-						
-					}
-				}
-				
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				return null;
-			}
-			return calendriers;
-			
-		}
 
 
 }
