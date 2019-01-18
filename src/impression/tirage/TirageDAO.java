c26d4077c43f1f7a167ccac84079090854ce952e
guevarat <t.guevara@laposte.net>
2019-01-18 15:22:22 +0100

    TirageDAO avancement

--- src/impression/tirage/TirageDAO.java
+++ src/impression/tirage/TirageDAO.java
@@ -1,12 +1,18 @@
 package src.impression.tirage;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class TirageDAO {
 
-	
+	public static void createTirage(Connection c, int idImp) throws SQLException {
+		Statement stat= c.createStatement();
+		String query= "insert into adresse (idImp)  values ('"+idImp+"')";
+		stat.executeUpdate(query);
+	}
 	
 	public static ArrayList<Tirage> getTirages(ResultSet result) {
 		ArrayList<Tirage> Tirage = new ArrayList<Tirage>();
