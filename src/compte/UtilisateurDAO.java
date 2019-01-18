931dac299dc1c99ef066e7f31cd730a5326b9183
guevarat <t.guevara@laposte.net>
2019-01-18 11:47:54 +0100

    Mettre en public les différentes void de: AdresseDAO, AlbumDAO, UtilisateurDao

--- src/compte/UtilisateurDAO.java
+++ src/compte/UtilisateurDAO.java
@@ -5,43 +5,43 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class UtilisateurDAO {
-	static void createUtilisateur(Connection c, String nom, String prenom, String mdp, String email, Boolean active, Statut statut) throws SQLException {
+	public static void createUtilisateur(Connection c, String nom, String prenom, String mdp, String email, Boolean active, Statut statut) throws SQLException {
 		Statement stat= c.createStatement();
 		String query= "insert into Utilisateur (nom , prenom, mdp , email, active, statut) values ('"+nom+"','"+prenom+"','"+mdp+"','"+email+"','"+active+"','"+statut+"')";
 		stat.executeUpdate(query);
 	}
 	
-	static void selectAll(Connection c) throws SQLException {
+	public static void selectAll(Connection c) throws SQLException {
 		Statement stat= c.createStatement();
 		String query= "select * from Utilisateur";
 		stat.executeUpdate(query);
 	}
 	
-	static void selectAllFromUser(Connection c, int idu) throws SQLException {
+	public static void selectAllFromUser(Connection c, int idu) throws SQLException {
 		Statement stat= c.createStatement();
 		String query= "select * from Utilisateur where idUser='"+idu+"' ";
 		stat.executeUpdate(query);
 	}
 	
-	static void selectAllUserFromStatut(Connection c, Statut statut) throws SQLException {
+	public static void selectAllUserFromStatut(Connection c, Statut statut) throws SQLException {
 		Statement stat= c.createStatement();
 		String query= "select * from Utilisateur where statut='"+statut+"'";
 		stat.executeUpdate(query);
 	}
 	
-	static void selectAllUserFromActive(Connection c, Boolean active) throws SQLException {
+	public static void selectAllUserFromActive(Connection c, Boolean active) throws SQLException {
 		Statement stat= c.createStatement();
 		String query= "select * from Utilisateur where active='"+active+"'";
 		stat.executeUpdate(query);
 	}
 	
-	static void deleteUtilisateur(Connection c, int idUtilisateur) throws SQLException {
+	public static void deleteUtilisateur(Connection c, int idUtilisateur) throws SQLException {
 		Statement stat= c.createStatement();
 		String query= "update Utilisateur set active='"+false+"' where idUser='"+idUtilisateur+"' ";
 		stat.executeUpdate(query);
 	}
 	
-	static void updateUtilisateur(Connection c, int idUtilisateur, String nom, String prenom, String mdp, String email, Statut statut) throws SQLException {
+	public static void updateUtilisateur(Connection c, int idUtilisateur, String nom, String prenom, String mdp, String email, Statut statut) throws SQLException {
 		Statement stat= c.createStatement();
 		String query= "update Utilisateur set nom='"+nom+"',prenom='"+prenom+"',mdp='"+mdp+"',email='"+email+"', statut='"+statut+"' where idUser='"+idUtilisateur+"'";
 		stat.executeUpdate(query);
--- src/impression/album/AlbumDAO.java
+++ src/impression/album/AlbumDAO.java
@@ -7,31 +7,31 @@ import java.sql.Statement;
 import src.compte.Statut;
 
 public class AlbumDAO {
-	static void createAlbum(Connection c, int photo, String titre) throws SQLException {
+	public static void createAlbum(Connection c, int photo, String titre) throws SQLException {
 		Statement stat= c.createStatement();
 		String query= "insert into Album (photoCouv , titreCouv) values ('"+photo+"','"+titre+"')";
 		stat.executeUpdate(query);
 	}
 	
-	static void selectAll(Connection c) throws SQLException {
+	public static void selectAll(Connection c) throws SQLException {
 		Statement stat= c.createStatement();
 		String query= "select * from Album";
 		stat.executeUpdate(query);
 	}
 	
-	static void selectAllFromAlbum(Connection c, int idi) throws SQLException {
+	public static void selectAllFromAlbum(Connection c, int idi) throws SQLException {
 		Statement stat= c.createStatement();
 		String query= "select * from Album where idImp='"+idi+"' ";
 		stat.executeUpdate(query);
 	}
 	
-	static void deleteUtilisateur(Connection c, int idi) throws SQLException {
+	public static void deleteUtilisateur(Connection c, int idi) throws SQLException {
 		Statement stat= c.createStatement();
 		String query= "delete from 'Album' where 'idImp'='"+idi+"' ";
 		stat.executeUpdate(query);
 	}
 	
-	static void updateUtilisateur(Connection c, int idi, int photo, String titre) throws SQLException {
+	public static void updateUtilisateur(Connection c, int idi, int photo, String titre) throws SQLException {
 		Statement stat= c.createStatement();
 		String query= "update Album set photoCouv='"+photo+"' ,titreCouv='"+titre+"' where idImp='"+idi+"'";
 		stat.executeUpdate(query);
