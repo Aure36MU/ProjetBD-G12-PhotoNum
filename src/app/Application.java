4579ec31761946c1f6d9f53c8e8db4077ac1022c
allemanp <pauline71380@sfr.fr>
2019-01-24 11:52:43 +0100

    modification menu application.java et ajout de fonctions

--- src/app/Application.java
+++ src/app/Application.java
@@ -12,18 +12,18 @@ public class Application {
 	static String USER;
 	static String PASSWD;
 	static Connection c; 
-	
+
 	public static Statut choixStatut(){
 		System.out.println("Vous pouvez vous inscrire en temps que client ou en tant que gestionnaire.");
 		return Statut.valueOf(LectureClavier.lireChaine("CLIENT ou GESTIONNAIRE ?"));
 	}
-	
+
 	public static Utilisateur inscription(Connection c) throws SQLException{
 		System.out.println("***********************");
 		System.out.println("      INSCRIPTION ");
 		System.out.println("***********************");
 		System.out.println("Bienvenue sur le site PhotoNum ! Nous allons vous demander quelques infos pour la création de votre compte");
-		
+
 		Statut statut= choixStatut();			
 		String mail= LectureClavier.lireChaine("Votre adresse mail : ");	
 		String mdp= LectureClavier.lireChaine("Votre mot de passe : ");	
@@ -40,8 +40,8 @@ public class Application {
 		System.out.println("**********************");
 
 		String mail = LectureClavier.lireChaine("Pour vous connecter, saisissez votre mail : ");
-    	Utilisateur utilisateur = UtilisateurDAO.selectUserWithMail(c, mail);
-    	String mdp = null;
+		Utilisateur utilisateur = UtilisateurDAO.selectUserWithMail(c, mail);
+		String mdp = null;
 		while(!mdp.equals(utilisateur.getMdp())){
 			mdp = LectureClavier.lireChaine("Veuillez entrer le mot de passe correspondant ou entrez \"return to menu\" pour retourner au Menu Principal");
 			if(mdp.equals("return to menu")){
@@ -50,40 +50,71 @@ public class Application {
 		}
 		return utilisateur;
 	}
-	
+
 	public static void main(String[] args) throws SQLException {
 		try{
-			Utilisateur utilisateur = null;
 			USER=LectureClavier.lireChaine("saississez l'identifiant pour la connexion à la base : ");
 			PASSWD=LectureClavier.lireChaine("Quel est le mot de passe ? ");
-			
-			// Enregistrement du driver Oracle
-		    DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());  	    
-		    // Etablissement de la connection
-		    System.out.print("Connecting to the database... "); 
-		    c = DriverManager.getConnection(CONN_URL,USER,PASSWD);
-		    System.out.println("connected");
-		    System.out.println("**********************************************************************");
-		    
-		    System.out.println("    ");
-		    while(utilisateur == null){
-				System.out.println("Souhaitez-vous, vous connectez ou vous inscrire? ");
+
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());  	    
+			System.out.print("Connecting to the database... "); 
+			c = DriverManager.getConnection(CONN_URL,USER,PASSWD);
+			System.out.println("connected");
+			System.out.println("**********************************************************************");
+			System.out.println("    ");
+
+			Utilisateur utilisateur = null;
+			while(utilisateur == null){
+				System.out.println("Souhaitez-vous, vous connectez ou vous inscrire à PhotoNum ? ");
 				int choix = LectureClavier.lireEntier("tapez 1 pour vous connecter, ou tapez 2 pour vous inscrire");
-				
+
 				switch(choix){ 
+				case 1:  
+					utilisateur = connexion(c);
+					System.out.println("Vous etes maintenant connecté.");
+					break;
+				case 2:
+					utilisateur = inscription(c);
+					System.out.println("Vous avez été inscrit.");
+					break;
+				default : System.out.println("Veuillez faire un choix. ");
+				}
+
+				while(utilisateur != null){
+					System.out.println("*****************************************************************************");
+					System.out.println("Que voulez vous faire ?");
+					System.out.println("1 : Se déconnecter.");
+					System.out.println("2 : gerer mes impressions.");
+					System.out.println("3 : gerer mes fichiers images.");
+					int choixAction = LectureClavier.lireEntier("4 : voir mon historique de commandes.");
+
+					switch(choixAction){ 
 					case 1:  
-						utilisateur = connexion(c);
+						utilisateur = null;
+						System.out.println("Vous avez été déconnecté");
 						break;
 					case 2:
 						utilisateur = inscription(c);
+						System.out.println("Vous avez été inscrit.");
+						break;
+					case 3:
+						utilisateur = inscription(c);
+						System.out.println("Vous avez été inscrit.");
+						break;
+					case 4:
+						utilisateur = inscription(c);
+						System.out.println("Vous avez été inscrit.");
 						break;
 					default : System.out.println("Veuillez faire un choix. ");
-					
-				}		
-		    }	
-			
-			
-			
+					}
+				}	
+			} 
+
+
+
+
+
+
 			/*
 			 * Partie de code pour charger un jeu de test 
 			 * 
@@ -100,9 +131,9 @@ public class Application {
 			 * }
 			 */
 		}
-	 catch (Exception e) {
-		e.printStackTrace();
-	}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
