bb2159fb8debfd1d7fb58dac8f2a55a40e70fddf
Aure36MU <32073804+Aure36MU@users.noreply.github.com>
2019-01-25 17:10:37 +0100

    (fix) Modele.java : Correction méthode definir()

--- src/impression/Modele.java
+++ src/impression/Modele.java
@@ -13,10 +13,18 @@ public enum Modele {
 	AUCUN;
 	
 	public static String definir() {
-		int choix = LectureClavier.lireEntier("Choisissez un modèle de calendrier : 1) BUREAU ; 2) MURAL ; 3) BOIS ; 4) ALUMINIUM ; 5) CARBONE ; 6) JOURNALIER ; 7) SEMAINIER ; 8) AUCUN");
-		while (Modele.values()[choix-1] == null) {
+
+		StringBuilder invite = new StringBuilder("Choisissez un modèle d'impression : ");
+		int index=1;
+		for(Modele i : Modele.values()) {
+			invite.append(index + ") " + i + " ; ");
+			index += 1;
+		}
+		invite.setLength(invite.length()-2);
+		int choix = LectureClavier.lireEntier(""+invite);
+		while (choix > Modele.values().length || choix <= 0) {
 			if (choix == 0) { return null; }
-			choix = LectureClavier.lireEntier("Choix incorrect. Choisissez un modèle de calendrier : 1) BUREAU ; 2) MURAL ; 3) BOIS ; 4) ALUMINIUM ; 5) CARBONE ; 6) JOURNALIER ; 7) SEMAINIER ; 8) AUCUN");
+			choix = LectureClavier.lireEntier("Choix incorrect. "+invite);
 		}
 		return Modele.values()[choix-1].toString();
 	}
