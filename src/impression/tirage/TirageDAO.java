f32eb9239e910fb7a88495a644816fbd9d5c1168
allemanp <pauline71380@sfr.fr>
2019-01-30 10:20:18 +0100

    refactoring terminé

--- src/impression/tirage/TirageDAO.java
+++ src/impression/tirage/TirageDAO.java
@@ -6,12 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-
-
 public class TirageDAO {
 
-
-	
 	public static ArrayList<Tirage> selectAll(Connection c) throws SQLException {
 		Statement stat = c.createStatement();
 		ResultSet result = stat.executeQuery("select * from Tirage");
@@ -20,7 +16,6 @@ public class TirageDAO {
 	
 	/**
 	 * S�lectionne tous les tirages avec des conditions param�tr�es.
-	 * 
 	 * @param conn Connection SQL
 	 * @param condition cha�ne de caract�res format� comme suit : "condition1 {AND condition2}"
 	 * Exemple : "foo=1 AND bar='bar' AND truc<>42"
@@ -28,12 +23,9 @@ public class TirageDAO {
 	 * @throws SQLException 
 	 */
 	public static ArrayList<Tirage> selectAll(Connection conn, String condition) throws SQLException {
-
 		Statement state = conn.createStatement();
 		ResultSet result = state.executeQuery("SELECT * FROM Tirage WHERE "+condition);
 		return getTirages(result);
-
-		
 	}
 	
 	public static ArrayList<Tirage> selectAllFromUser(Connection c, int idUser) throws SQLException {
@@ -51,38 +43,23 @@ public class TirageDAO {
 	
 	/**
 	 * Ajoute un tirage dans la base.
-	 * 
 	 * @param id id impression
 	 * @throws SQLException 
 	 */
 	public static void insertTirage(Connection conn, int id) throws SQLException {
-
 		Statement state = conn.createStatement();
 		state.executeUpdate("INSERT INTO Tirage VALUES("+id+")");
-		
 	}
-	
-	
-	/**
-	 * updateTirage() : est inutile car il n'y a aucun attribut � modifier � part l'idImp
-	 * 
-	 */
-	public static void updateTirage() {}
-	
 	
 	/**
 	 * Supprime un tirage d'un idImp donn� de la base.
-	 * 
 	 * @param id id impression
 	 * @throws SQLException 
 	 */
 	public static void deleteTirage(Connection conn, int id) throws SQLException {
-
 		Statement state = conn.createStatement();
 		state.executeUpdate("DELETE FROM Tirage WHERE idImp="+id);
-		
 	}
-	
 	
 	public static ArrayList<Tirage> getTirages(ResultSet result) {
 		ArrayList<Tirage> tirages = new ArrayList<Tirage>();
