553048344d4e035b7a61f6bf22c4ee68e9c754f5
sly <alpensly@gmail.com>
2019-01-30 10:23:35 +0100

    push peut etre fatal

--- Connexion_Base_Oracle/selectBase.sql
+++ Connexion_Base_Oracle/selectBase.sql
@@ -1,11 +1,6 @@
-select * from commande
-SELECT * from impression
-insert into Article (prix, qte, idImp, idComm)  values (20, 1,42,1)
-select * from Utilisateur where email = 'a@b.com'
-select * from tirage
-SELECT max(idImp) FROM Impression
-SELECT max(idUser) FROM Utilisateur
-select * from Impression where type='CALENDRIER'
-(SELECT idImp FROM Impression i WHERE i.idUser=2 and i.type='CALENDRIER') MINUS (SELECT idImp FROM Article NATURAL JOIN Impression WHERE type='CALENDRIER')
-SELECT table_name FROM user_tables
-
+select * from CodePromo
+insert into CodePromo (code,taux, idUser) values ('noyeux',10,21)
+insert into CodePromo (code,taux, idUser) values ('joel',10,21)
+insert into CodePromo (code,taux, idUser) values ('pika',10,21)
+insert into CodePromo (code,taux, idUser) values ('chu',10,21)
+select * from CodePromo
